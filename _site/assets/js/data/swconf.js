e4e38fd6ca67b68bcd9b9089346710d63fe1a490
Carl Valenzuela <carleliasvalenzuela@gmail.com>
2025-03-11 09:21:39 +0000

    Update .gitignore file

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,28 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1741683917',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
